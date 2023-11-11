e8737c6ca2eb943add236f80710c31332c53c5a2
Mauricio Cacuango <amcacuangoa@uce.edu.ec>
2023-11-10 19:06:38 -0500

    Taller 4

--- src/main/java/com/uce/edu/Pa2U1P5AcApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5AcApplication.java
@@ -17,6 +17,7 @@ public class Pa2U1P5AcApplication implements CommandLineRunner
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
 		System.out.println("Soy Mauricio Cacuango");
+		System.out.println("Programacion Avanzada 2 desde la rama main");
 	}
 
 	
