b7614208dd2a166e4dc7e9f35705324d1f60b03d
Mauricio Cacuango <amcacuangoa@uce.edu.ec>
2023-11-13 19:41:44 -0500

    Taller 5 IoC

--- src/main/java/com/uce/edu/Pa2U1P5AcApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5AcApplication.java
@@ -1,22 +1,32 @@
 package com.uce.edu;
 
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.uce.edu.ioc.di.Estudiante;
 
 @SpringBootApplication
 public class Pa2U1P5AcApplication implements CommandLineRunner
 {
 
+	@Autowired
+	public Estudiante estudiante;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5AcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
-		System.out.println("Soy Mauricio Cacuango");
+		this.estudiante.setNombre("Mauricio");
+		this.estudiante.setApellido("Cacuango");
+		this.estudiante.setCedula("1723524664");
+		this.estudiante.setSalario(new BigDecimal(100));
+		System.out.println(estudiante);
 	}
 
 	
