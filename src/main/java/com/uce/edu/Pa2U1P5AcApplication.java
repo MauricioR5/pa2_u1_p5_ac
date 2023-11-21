7e5f9ce31ec090385cba6cba63c6c8a66c2bbe72
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-11-20 19:51:24 -0500

    Taller 8

--- src/main/java/com/uce/edu/Pa2U1P5AcApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5AcApplication.java
@@ -1,24 +1,25 @@
 package com.uce.edu;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.uce.edu.repository.modelo.Materia;
-import com.uce.edu.service.IMateriaService;
 
 @SpringBootApplication
 public class Pa2U1P5AcApplication implements CommandLineRunner
 {
 
-	@Autowired
-	private IMateriaService iMateriaService;
 	
 	@Autowired
 	private Materia materia;
+	
+	@Autowired
+	private Materia materia1;
+	
+	@Autowired
+	private Materia materia2;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5AcApplication.class, args);
@@ -26,38 +27,25 @@ public class Pa2U1P5AcApplication implements CommandLineRunner
 
 	@Override
 	public void run(String... args) throws Exception {
-		this.materia.setCodigo("M1");
-		this.materia.setNombre("P. Avanzada");
-		this.materia.setNumeroCreditos(10);
+	
+		this.materia.setNombre("Avanzada II");
+		System.out.println(this.materia);
 		
-		this.iMateriaService.registrar(materia);
+		System.out.println(this.materia1);
+
+		this.materia1.setNombre("Nuevo nombre");
+		System.out.println(this.materia1);
+		System.out.println(this.materia);
 		
-		System.out.println("Reporte Inicio");
 		
-		List<Materia> reporte = this.iMateriaService.buscarTodos();
-		for(Materia mat:reporte) {
-			System.out.println(mat);
+		this.materia2.setNombre("Nombre final");
+		System.out.println(this.materia2);
+		System.out.println(this.materia1);
+		System.out.println(this.materia);
+
+
+
+			
 		}
-		
-		System.out.println("Reporte Fin");
-
-		
-		Materia mate = this.iMateriaService.buscar("M1");
-		System.out.println(mate);
-	
-		mate.setNumeroCreditos(20);
-		
-		this.iMateriaService.actualizar(mate);
-		Materia mate2 = this.iMateriaService.buscar("M1");//imprime el "mate" de la base
-		System.out.println(mate2);
-		
-		this.iMateriaService.eliminar("M1");
-		
-		mate2 = this.iMateriaService.buscar("M1");
-		System.out.println(mate2);
-		
-	}
-
-	
 }
 
